8db1deff24e70d1c31cca1db0ed3400e82a840bc
Nurgaliev <catshomee@gmail.com>
2018-12-27 12:07:26 +0300

    add home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,88 @@
 <template>
-    <v-app>
-        <v-content>
-            <v-container>
-                <v-layout style="color:#009688;font-size:20px" align-center justify-center row fill-height>
-                    MAX SCORE AMONG ALL USERS - {{  highScore }}
+    <v-app light>
+        <section>
+            <v-parallax :src="require('@/assets/hero.jpeg')" height="600">
+                <v-layout column align-center justify-center class="white--text">
+                    <h1 class="white--text mb-2 display-1 text-xs-center">EnWord</h1>
+                    <v-btn class="teal lighten-2 mt-5" dark large href="/start">
+                        Get Started
+                    </v-btn>
                 </v-layout>
-            </v-container>
-        </v-content>
+            </v-parallax>
+        </section>
+
+        <section>
+            <v-layout column wrap class="my-5" align-center>
+                <v-flex xs12 sm4 class="my-3">
+                    <div class="text-xs-center">
+                        <v-container>
+                            <v-layout style="font-size:20px" class="blue--text" align-center justify-center row fill-height>
+                                MAX SCORE AMONG ALL USERS - {{ highScore }}
+                            </v-layout>
+                        </v-container>
+                    </div>
+                </v-flex>
+                <v-flex xs12>
+                    <v-container grid-list-xl>
+                        <v-layout row wrap align-center>
+                            <v-flex xs12 md4>
+                                <v-card class="elevation-0 transparent">
+                                    <v-card-text class="text-xs-center">
+                                        <v-icon x-large class="blue--text text--lighten-2">color_lens</v-icon>
+                                    </v-card-text>
+                                    <v-card-title primary-title class="layout justify-center">
+                                        <div class="headline text-xs-center">Material Design</div>
+                                    </v-card-title>
+                                    <v-card-text>
+                                        Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse
+                                        potenti.
+                                    </v-card-text>
+                                </v-card>
+                            </v-flex>
+                            <v-flex xs12 md4>
+                                <v-card class="elevation-0 transparent">
+                                    <v-card-text class="text-xs-center">
+                                        <v-icon x-large class="blue--text text--lighten-2">flash_on</v-icon>
+                                    </v-card-text>
+                                    <v-card-title primary-title class="layout justify-center">
+                                        <div class="headline">Fast development</div>
+                                    </v-card-title>
+                                    <v-card-text>
+                                        Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse
+                                        potenti.
+                                    </v-card-text>
+                                </v-card>
+                            </v-flex>
+                            <v-flex xs12 md4>
+                                <v-card class="elevation-0 transparent">
+                                    <v-card-text class="text-xs-center">
+                                        <v-icon x-large class="blue--text text--lighten-2">build</v-icon>
+                                    </v-card-text>
+                                    <v-card-title primary-title class="layout justify-center">
+                                        <div class="headline text-xs-center">Completely Open Sourced</div>
+                                    </v-card-title>
+                                    <v-card-text>
+                                        Cras facilisis mi vitae nunc lobortis pharetra. Nulla volutpat tincidunt ornare. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam in aliquet odio. Aliquam eu est vitae tellus bibendum tincidunt. Suspendisse
+                                        potenti.
+                                    </v-card-text>
+                                </v-card>
+                            </v-flex>
+                        </v-layout>
+                    </v-container>
+                </v-flex>
+            </v-layout>
+        </section>
+
     </v-app>
 </template>
 
 <script>
     export default {
+        data() {
+            return {
+                title: 'Your Logo'
+            }
+        },
         computed: {
             highScore() {
                 return this.$store.getters.gethighScore
