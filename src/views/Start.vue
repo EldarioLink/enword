b1e590ab98f67db7ede8d5019bb4c79d74242b9e
Nurgaliev <catshomee@gmail.com>
2019-01-05 16:45:41 +0300

    change button - add words

--- src/views/Start.vue
+++ src/views/Start.vue
@@ -23,13 +23,33 @@
         </v-layout>
       </v-container>
       <v-layout justify-end>
-       <v-btn
-        slot="activator"
-        color="red lighten-2"
-        dark
-      >
-        Click Me
-      </v-btn>
+        <div class="text-xs-center">
+          <v-dialog v-model="dialog" width="500">
+            <v-btn slot="activator" fab dark color="indigo">
+              <v-icon dark>add</v-icon>
+            </v-btn>
+
+            <v-card>
+              <v-card-title class="headline grey lighten-2" primary-title>
+                Privacy Policy
+              </v-card-title>
+
+              <v-card-text>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </v-card-text>
+
+              <v-divider></v-divider>
+
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary" flat @click="dialog = false">
+                  I accept
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+        </div>
       </v-layout>
     </v-content>
   </v-app>
@@ -46,7 +66,8 @@
         seconds: 59,
         setInt: true,
         showHelp: false,
-        timeinterval: undefined
+        timeinterval: undefined,
+        dialog: false
       };
     },
     methods: {
